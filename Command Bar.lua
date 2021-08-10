9384aeee1db8a45e36c2855884d306374951673c
emwie <74454528+emwie@users.noreply.github.com>
2021-08-10 13:34:04 -0400

    hi

--- Command Bar.lua
+++ Command Bar.lua
@@ -1,22 +1,18 @@
-local Name = game.Players.LocalPlayer.Name
-local CmdGui = Instance.new("ScreenGui", MainGui)
+local NecrokiCmdGui = Instance.new("ScreenGui", MainGui)
 local CmdBox = Instance.new("TextBox")
 local AdminBar = CmdBox.FocusLost
-local PrefixOpener = ";" -- not changing this prolly ? 
+local PrefixOpener = ";" --cmdbar opener?? 
 local Prefix = PrefixOpener
-local Player = game:GetService("Players").LocalPlayer
-local KeyPress = Player:GetMouse()
-local added = game.Workspace.ChildAdded
-local hum = Player.Character.Humanoid
-local m = Player:GetMouse()
-local uis = game:GetService('UserInputService')
-CmdGui.Name = "CmdGui"
-CmdGui.Parent = game.CoreGui
+local lp = game:GetService("Players").LocalPlayer
+local KeyPress = lp:GetMouse()
+local m = lp:GetMouse()
+NecrokiCmdGui.Name = "NecrokiCmdGui"
+NecrokiCmdGui.Parent = game.CoreGui
 
 CmdBox.Name = "CmdBox"
-CmdBox.Parent = CmdGui
+CmdBox.Parent = NecrokiCmdGui
 CmdBox.BackgroundColor3 = Color3.fromRGB(33, 33, 33)
-CmdBox.BorderColor3 = Color3.fromRGB(255, 255, 255)
+CmdBox.BorderColor3 = Color3.fromRGB(244,134,122)
 CmdBox.BorderSizePixel = 3
 CmdBox.Position = UDim2.new(0, 0, 0.533333333333, 0)
 CmdBox.Size = UDim2.new(0, 0, 0, 20)
@@ -27,28 +23,28 @@ CmdBox.TextColor3 = Color3.fromRGB(255, 255, 255)
 CmdBox.TextSize = 15
 CmdBox.Visible = false
 KeyPress.KeyDown:connect(function(key)
-	if key == Prefix then
-		wait()
-		CmdBox.Text = ""
-		CmdBox.Visible = true
-		CmdBox:CaptureFocus()
-		CmdBox:TweenPosition(UDim2.new(0, 0, 0.533333333333, 0), "Out", "Quad", 0.3, true)
-		CmdBox:TweenSize(UDim2.new(0, 227,0, 27), "Out", "Quad", 0.3, true)
-		CmdBox.Text = ""
-		wait()
-		CmdBox.Text = ""
-	end
+    if key == Prefix then
+        wait()
+        CmdBox.Text = ""
+        CmdBox.Visible = true
+        CmdBox:CaptureFocus()
+        CmdBox:TweenPosition(UDim2.new(0, 0, 0.533333333333, 0), "Out", "Quad", 0.3, true)
+        CmdBox:TweenSize(UDim2.new(0, 227,0, 27), "Out", "Quad", 0.3, true)
+        CmdBox.Text = ""
+        wait()
+        CmdBox.Text = ""
+    end
 end)
 CmdBox.FocusLost:Connect(function(PressedEnter)
-	CmdBox:TweenSize(UDim2.new(0,0,0,0),"Out","Quad",0.5,false)
-	CmdBox:TweenPosition(UDim2.new(0,0,0), "Out", "Quad", 0.3, false)
-	if PressedEnter then
-		wait()
-		CmdBox.Text = ""
-		CmdBox.Visible = false
-	else
-		wait()
-		CmdBox.Text = ""
-		CmdBox.Visible = false
-	end
+    CmdBox:TweenSize(UDim2.new(0,0,0,0),"Out","Quad",0.5,false)
+    CmdBox:TweenPosition(UDim2.new(0,0,0), "Out", "Quad", 0.3, false)
+    if PressedEnter then
+        wait()
+        CmdBox.Text = ""
+        CmdBox.Visible = false
+    else
+        wait()
+        CmdBox.Text = ""
+        CmdBox.Visible = false
+    end
 end)
