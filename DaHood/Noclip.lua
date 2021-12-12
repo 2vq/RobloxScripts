7c26ed61144f219b433f652a7d093d570c3b5340
cyberbully <74454528+cyberbully@users.noreply.github.com>
2021-12-11 19:15:14 -0500

    Update Noclip.lua

--- DaHood/Noclip.lua
+++ DaHood/Noclip.lua
@@ -18,7 +18,6 @@ local RService = game:GetService("RunService")
 local Input = game:GetService("UserInputService")
 local Client = game:GetService("Players").LocalPlayer
 local Noclip = false
-local a=getrawmetatable(game)or debug.getrawmetatable(game)or getmetatable(game)if setreadonly then setreadonly(a,false)else make_writeable(a,true)end;local b=a.__index;local c=a.__newindex;local d=a.__namecall;local e=newcclosure or read_me;local f=getcallingscript or get_calling_script;local g=checkcaller or is_protosmasher_caller;a.__newindex=e(function(self,h,i)if checkcaller()then return c(self,h,i)end;if self:IsDescendantOf(Client.Character)and self.Name=="HumanoidRootPart"or self.Name=="Torso"then if h=="CFrame"or h=="Position"or h=="Anchored"then return nil end end;return c(self,h,i)end)if setreadonly then setreadonly(a,true)else make_writeable(a,false)end
 
 --[[ Start of NoClip ]]--
 RService.Stepped:Connect(
